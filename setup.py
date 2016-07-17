e237a6f4751c3b1c484c41014b03951e287e5a1f
zartstrom <phj.hitzler@gmail.com>
2016-07-17 16:02:06 +0200

    added license and set version to 0.0.1

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from distutils.core import setup
 setup(
     name='snaptime',
     packages=['snaptime'],  # this must be the same as the name above
-    version='0.1',
-    description='Transform and truncate timestamps with a short syntax',
+    version='0.0.1',
+    description='Transform timestamps with a simple syntax',
     install_requires=['python-dateutil'],
     author='Philipp Hitzler',
     author_email='phj.hitzler@gmail.com',
     url='https://github.com/zartstrom/snaptime',  # use the URL to the github repo
-    download_url='https://github.com/zartstrom/snaptime/tarball/0.1',
+    download_url='https://github.com/zartstrom/snaptime/tarball/0.0.1',
     keywords=['snap', 'datetime', 'date', 'truncate', 'transform'],  # arbitrary keywords
     classifiers=[],
 )
