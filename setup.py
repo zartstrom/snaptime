0eb210bf81149293891a735f2d6890593a28e893
zartstrom <phj.hitzler@gmail.com>
2017-06-08 16:09:56 +0200

    increased version

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from distutils.core import setup
 setup(
     name='snaptime',
     packages=['snaptime'],  # this must be the same as the name above
-    version='0.2.1',
-    description='Transform timestamps with a simple syntax "@day+9h"',  # think of including iconic '+1d@d' or similar
+    version='0.2.2',
+    description='Transform timestamps with a simple DSL',  # think of including iconic '+1d@d' or similar
     install_requires=['python-dateutil', 'pytz'],
     author='Philipp Hitzler',
     author_email='phj.hitzler@gmail.com',
     url='https://github.com/zartstrom/snaptime',  # use the URL to the github repo
-    download_url='https://github.com/zartstrom/snaptime/tarball/0.2.0',
+    download_url='https://github.com/zartstrom/snaptime/tarball/0.2.2',
     keywords=['snap', 'datetime', 'date', 'truncate', 'transform'],
     classifiers=[],
 )
